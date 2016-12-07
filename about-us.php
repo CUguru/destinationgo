a7eeefb654a1eb77b9e2f24cfc0da449151e48ee
apple <thenicoleyu@gmail.com>
2016-12-07 10:00:42 -0500

    no message

---
+++ about-us.php
@@ -0,0 +1,29 @@
+<!------------ HEADER & NAVIGATION SECTION ------------>
+
+<?php require 'includes/header.php' ?>
+
+<?php require 'includes/nav.php' ?>
+
+<!------------ ABOUT US SECTION ------------>
+
+<main class='section' id='about-us-section'>
+    <h1>About Us</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc malesuada neque orci, vitae rutrum enim tincidunt a. Maecenas quis ex mattis, pharetra ex non, porta sapien. Fusce sagittis placerat purus, ac convallis purus accumsan non. Sed ipsum magna, viverra ac tincidunt in, pretium a ipsum. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
+
+    <p>Aenean lacinia purus non tristique interdum. Mauris laoreet malesuada consectetur. In cursus eros eu ligula tristique egestas. Mauris pellentesque ante et cursus lacinia. Vivamus hendrerit sagittis ultricies. Nam hendrerit est nec fringilla sollicitudin. Praesent non commodo nisi. Pellentesque non justo magna. Curabitur in convallis neque, in consectetur nibh. Sed bibendum felis sit amet venenatis vestibulum. Suspendisse eleifend nulla eu feugiat bibendum. Aenean fermentum odio id ante placerat molestie. Pellentesque pharetra purus eget fringilla vestibulum. Suspendisse odio magna, sodales sit amet orci eu, sagittis vehicula tellus. Maecenas ut facilisis augue.</p>
+
+    <p>Nullam commodo volutpat nunc sit amet aliquet. Morbi vitae turpis diam. Nullam volutpat, tellus sed egestas euismod, eros sem eleifend lorem, commodo maximus dolor eros nec eros. Phasellus in odio nec est maximus molestie. Proin id risus bibendum, facilisis metus eu, fringilla lorem. Nam tortor purus, commodo id pulvinar quis, tincidunt id justo. Aliquam tempor luctus pretium. Sed orci ipsum, pulvinar sagittis orci eu, suscipit molestie odio. Phasellus mollis mauris quis facilisis aliquet. Mauris quis semper ipsum, non scelerisque orci. Praesent dapibus dolor purus, sed laoreet leo scelerisque ut. Cras felis lorem, ullamcorper eu nulla vitae, semper varius nunc. Nunc non egestas mauris. Quisque lacinia elit vitae mattis scelerisque. Integer ultricies magna at sem rhoncus, vel euismod ex vulputate. Donec scelerisque blandit enim, eu porttitor odio gravida id.</p>  
+</main> 
+
+<!------------ NEWSLETTER SECTION ------------>
+
+<?php require 'includes/newsletter.php' ?>
+
+<!------------ CONTACT SECTION ------------>
+
+<?php require 'includes/contact.php' ?>
+
+<!------------ FOOTER SECTION ------------>
+
+<?php require 'includes/footer.php' ?>
