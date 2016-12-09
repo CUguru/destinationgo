8b83364f43c70d027d08d8ea365f7091f4b57fdb
Nicole Yu <thenicoleyu@gmail.com>
2016-12-08 23:08:40 -0500

    Styled multiple pages with classes that are used by multiple pages.

--- reviews.php
+++ reviews.php
@@ -9,43 +9,52 @@
 <!------------ REVIEWS SECTION ------------>
 
 <main class='section' id='reviews-section'>
-    <h1>Reviews</h1>
     
-    <div class='location-container'>
-        <div class='location-image-container'>
-            <img src='images/placeholder.png'/>
-        </div>
-        <div class='location-text-container'>
-            <h3>Title</h3>
-            <h4>Location Name | Date</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
-            <a href='#'><div class='reviews-read-more'>Read More</div></a>
-        </div>
+    <div class='title-bg'>
+        <h1>Reviews</h1>
+        <img src='images/title_bg.png'/>
     </div>
     
-    <div class='location-container'>
-        <div class='location-image-container'>
-            <img src='images/placeholder.png'/>
-        </div>
-        <div class='location-text-container'>
-            <h3>Title</h3>
-            <h4>Location Name | Date</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
-            <a href='#'><div class='read-more reviews-read-more'>Read More</div></a>
-        </div>
-    </div>
+    <div class='section-bg' id='reviews-bg'>
     
-    <div class='location-container'>
-        <div class='location-image-container'>
-            <img src='images/placeholder.png'/>
-        </div>
-        <div class='location-text-container'>
-            <h3>Title</h3>
-            <h4>Location Name | Date</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
-            <a href='#'><div class='reviews-read-more'>Read More</div></a>
-        </div>
-    </div>
+        <div class='container reviews-container'>
+            <div class='image-container'>
+                <img src='images/placeholder.png'/>
+            </div>
+            <div class='text-container'>
+                <h2>Title</h2>
+                <h3>Location Name | Date</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
+                <a href='#'><div class='read-more'>Read More</div></a>
+            </div> <!-- end text-container -->
+        </div> <!-- end reviews-container -->
+
+        <div class='container reviews-container'>
+            <div class='image-container'>
+                <img src='images/placeholder.png'/>
+            </div>
+            <div class='text-container'>
+                <h2>Title</h2>
+                <h3>Location Name | Date</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
+                <a href='#'><div class='read-more'>Read More</div></a>
+            </div> <!-- end text-container -->
+        </div> <!-- end reviews-container -->
+
+        <div class='container reviews-container'>
+            <div class='image-container'>
+                <img src='images/placeholder.png'/>
+            </div>
+            <div class='text-container'>
+                <h2>Title</h2>
+                <h3>Location Name | Date</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
+                <a href='#'><div class='read-more'>Read More</div></a>
+            </div> <!-- end text-container -->
+        </div> <!-- end reviews-container -->
+    
+    </div> <!-- end reviews-bg -->
+    
 </main> <!-- end main-section -->
 
 <!------------ FOOTER SECTION ------------>
--- search_results.php
+++ search_results.php
@@ -13,28 +13,28 @@
         <img src='images/title_bg.png'/>
     </div>
     
-    <div id='search-results-bg'>
+    <div class='section-bg' id='search-results-bg'>
         
-        <div class='results-container'>
-            <div class='results-image-container'>
+        <div class='container results-container'>
+            <div class='image-container'>
                 <img src='images/placeholder.png'/>
             </div> <!-- end results-image -->
-            <div class='results-text'>
+            <div class='text-container'>
                 <h2>Lorem Ipsum</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
                 <a href='#'><div class='read-more'>READ MORE</div></a>
-            </div> <!-- end results-text -->
+            </div> <!-- end text-container -->
         </div> <!-- end results-container -->
         
-        <div class='results-container'>
-            <div class='results-image-container'>
+        <div class='container results-container'>
+            <div class='image-container'>
                 <img src='images/placeholder.png'/>
             </div> <!-- end results-image -->
-            <div class='results-text'>
+            <div class='text-container'>
                 <h2>Lorem Ipsum</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
                 <a href='#'><div class='read-more'>READ MORE</div></a>
-            </div> <!-- end results-text -->
+            </div> <!-- end text-container -->
         </div> <!-- end results-container -->
         
     </div> <!-- end search-results-bg -->
