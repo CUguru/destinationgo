7495ec2a500f8780195b3839788db376d9c34626
Nicole Yu <thenicoleyu@gmail.com>
2016-12-08 20:51:36 -0500

    no message

--- includes/nav.php
+++ includes/nav.php
@@ -1,3 +1,23 @@
+<?php 
+
+$file = $_SERVER['PHP_SELF'];
+
+$start = strrpos($file, '/') + 1; // string position of '/' on $file plus one
+$end = strrpos($file, '.');
+$len = $end - $start;
+$page_name = substr($file, $start, $len);
+$page_name = str_replace("_", " ", $page_name);
+
+//echo strtolower($page_name);
+
+function isCurrentPage($page) {
+    global $page_name;
+    if (strtolower($page_name) == $page) {
+        echo "id='current'";
+    } 
+}
+?>
+
 <nav id='navigation'>
     <ul id='log-in'>
         <a href='#'><li>Sign Up</li></a>
@@ -10,9 +30,9 @@
         </div>
     </a>
     <ul id='main-nav'>
-        <a href='about_us.php'><li>About Us</li></a>
+        <a href='about_us.php'><li <?php isCurrentPage('about us')?> >About Us</li></a>
         <a href='default.php#feat-dest-section'><li>Featured Locations</li></a>
-        <a href='reviews.php'><li>Reviews</li></a>
+        <a href='reviews.php'><li <?php isCurrentPage('reviews')?> >Reviews</li></a>
         <a href='default.php#contact-section'><li>Contact Us</li></a>
     </ul>
 </nav>
