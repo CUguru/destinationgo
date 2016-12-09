91e0b0bb32551b75b30e81ec990b72c8caaaddd8
Nicole Yu <thenicoleyu@gmail.com>
2016-12-08 21:36:18 -0500

    no message

--- default.php
+++ default.php
@@ -1,86 +1,110 @@
 <!-- HEADER SECTION -->
 
-<!-- <?php require 'includes/header.php' ?> -->
-        
+<?php require 'includes/header.php' ?> 
+
 <!-- NAVIGATION & PREFERENCES SECTION -->
 
 <main class='section' id='preferences-section'>
 
     <?php require 'includes/nav.php' ?>
-<!-- Will be changing the div categories to input boxes for the php -->
+    <!-- Will be changing the div categories to input boxes for the php -->
     <div id='preferences-container'>
         <h3>Pick Your Preferences</h3>
         <form id='preferences-form' action=''>
             <div class='categories'>
                 <div>
-                    <label><input type="radio" name="weather" value="beaches">Beaches</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Beaches</p>
+                    <label>
+                        <input type="radio" name="weather" value="beaches">
+                        <img src='images/placeholder.png'/>
+                        <p>Beaches</p>
+                    </label>
                 </div>
                 <div>
-                    <label><input type="radio" name="weather" value="mountains">Mountains</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Mountains</p>
-                </div>
-            </div> 
-            <div class='categories'>
-                <div>
-                    <label><input type="radio" name="activity" value="relaxing">Relaxing</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Relaxing</p>
-                </div>
-                <div>
-                    <label><input type="radio" name="activity" value="nightlife">Nightlife</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Nightlife</p>
+                    <label>
+                        <input type="radio" name="weather" value="mountains">
+                        <img src='images/placeholder.png'/>
+                        <p>Mountains</p>
+                    </label>
                 </div>
             </div> 
             <div class='categories'>
                 <div>
-                    <label><input type="radio" name="cost" value="high">High_End</label>
-                    <img src='images/placeholder.png'/>
-                    <p>High-End</p>
+                    <label>
+                        <input type="radio" name="activity" value="relaxing">
+                        <img src='images/placeholder.png'/>
+                        <p>Relaxing</p> 
+                    </label>
                 </div>
                 <div>
-                    <label><input type="radio" name="cost" value="low">Low-End</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Thrifty</p>
-                </div>
-            </div> 
-            <div class='categories'>
-                <div>
-                    <label><input type="radio" name="location" value="urban">Urban</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Urban</p>
-                </div>
-                <div>
-                    <label><input type="radio" name="location" value="rural">Rural</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Rural</p>
+                    <label>
+                        <input type="radio" name="activity" value="nightlife">
+                        <img src='images/placeholder.png'/>
+                        <p>Nightlife</p>
+                    </label>
                 </div>
             </div> 
             <div class='categories'>
                 <div>
-                    <label><input type="radio" name="family" value="kids">Kid-Friendly</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Kid-Friendly</p>
+                    <label>
+                        <input type="radio" name="cost" value="high">
+                        <img src='images/placeholder.png'/>
+                        <p>High-End</p>
+                    </label>
                 </div>
                 <div>
-                    <label><input type="radio" name="family" value="adults">Adults-Only</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Adults</p>
+                    <label>
+                        <input type="radio" name="cost" value="low">
+                        <img src='images/placeholder.png'/>
+                        <p>Thrifty</p>
+                    </label>
                 </div>
             </div> 
             <div class='categories'>
                 <div>
-                    <label><input type="radio" name="popularity" value="popular">Popular</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Popular</p>
+                    <label>
+                        <input type="radio" name="location" value="urban">
+                        <img src='images/placeholder.png'/>
+                        <p>Urban</p>
+                    </label>
                 </div>
                 <div>
-                    <label><input type="radio" name="popularity" value="hidden">Hidden Treasure</label>
-                    <img src='images/placeholder.png'/>
-                    <p>Hidden Treasures</p>
+                    <label>
+                        <input type="radio" name="location" value="rural">
+                        <img src='images/placeholder.png'/>
+                        <p>Rural</p>
+                    </label>
+                </div>
+            </div> 
+            <div class='categories'>
+                <div>
+                    <label>
+                        <input type="radio" name="family" value="kids">
+                        <img src='images/placeholder.png'/>
+                        <p>Kid-Friendly</p>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input type="radio" name="family" value="adults">
+                        <img src='images/placeholder.png'/>
+                        <p>Adults</p>
+                    </label>
+                </div>
+            </div> 
+            <div class='categories'>
+                <div>
+                    <label>
+                        <input type="radio" name="popularity" value="popular">
+                        <img src='images/placeholder.png'/>
+                        <p>Popular</p>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input type="radio" name="popularity" value="hidden">
+                        <img src='images/placeholder.png'/>
+                        <p>Hidden Treasures</p>
+                    </label>
                 </div>
             </div> 
         </form> <!-- end preferences-form -->
