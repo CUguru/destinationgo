e19e2dada6217641a4388939c217cc2e5e766729
hw0292 <hw0292@gmail.com>
2016-12-10 00:05:31 -0500

    Add files via upload
    search_results.php with updated css

--- search_results.php
+++ search_results.php
@@ -1,52 +1,63 @@
-<!------------ HEADER & NAVIGATION SECTION ------------>
+<!-- HEADER SECTION -->
 
-<?php require 'includes/header.php' ?>
+<?php require 'includes/header.php' ?> 
 
-<?php require 'includes/nav.php' ?>
+<!-- NAVIGATION & PREFERENCES SECTION -->
+    <?php require 'includes/nav.php' ?>
+
+
 
 <!------------ SEARCH RESULTS SECTION ------------>
 
-<main class='section' id='search-results-section'>  
+<main class='section entireFlax_item' id='search-results-section'>  
     
-    <div class='title-bg'>
-        <h1>Search Results</h1>
-        <img src='images/title_bg.png'/>
-    </div>
+    <!--<div class='title-bg'>-->
+        <h1 class='margin_b2'>Search Results</h1>
+    <!--</div>-->
     
-    <div class='section-bg' id='search-results-bg'>
-        
-        <div class='container results-container'>
-            <div class='image-container'>
-                <img src='images/placeholder.png'/>
+    <div id='search-results-bg'>
+        <div class="margin_b2"></div>
+        <div class='results-container'>
+            <div class='results-image-container'>
+                <img src='images/pic01.jpg'/>
             </div> <!-- end results-image -->
-            <div class='text-container'>
+            <div class='results-text'>
                 <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
-                <a href='#'><div class='read-more'>READ MORE</div></a>
-            </div> <!-- end text-container -->
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <a href='#'><div class='read-more button1'>READ MORE</div></a>
+            </div> <!-- end results-text -->
         </div> <!-- end results-container -->
         
-        <div class='container results-container'>
-            <div class='image-container'>
-                <img src='images/placeholder.png'/>
+        <div class='results-container'>
+            <div class='results-image-container'>
+                <img src='images/pic02.jpg'/>
             </div> <!-- end results-image -->
-            <div class='text-container'>
+            <div class='results-text'>
                 <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
-                <a href='#'><div class='read-more'>READ MORE</div></a>
-            </div> <!-- end text-container -->
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <a href='#'><div class='read-more button1'>READ MORE</div></a>
+            </div> <!-- end results-text -->
         </div> <!-- end results-container -->
+        
+        <div class='results-container'>
+            <div class='results-image-container'>
+                <img src='images/pic03.jpg'/>
+            </div> <!-- end results-image -->
+            <div class='results-text'>
+                <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <a href='#'><div class='read-more button1'>READ MORE</div></a>
+            </div> <!-- end results-text -->
+        </div> <!-- end results-container -->
+        
+        
         
     </div> <!-- end search-results-bg -->
 </main>
 
-<!------------ CONTACT SECTION ------------>
-
-<?php require 'includes/contact.php' ?>
-
-<!------------ FOOTER SECTION ------------>
-
-<script src="js/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
+<!-- FOOTER SECTION -->
 
 <?php require 'includes/footer.php' ?>
+
+
+
