6d05200a277009dadaebeb154e1a9e642286af3d
hw0292 <hw0292@gmail.com>
2016-12-11 00:04:53 -0500

    Add files via upload
    reviews.php with css

--- reviews.php
+++ reviews.php
@@ -8,51 +8,51 @@
 
 <!------------ REVIEWS SECTION ------------>
 
+<!------------ REVIEWS SECTION ------------>
+
 <main class='section' id='reviews-section'>
     
-    <div class='title-bg'>
-        <h1>Reviews</h1>
-        <img src='images/title_bg.png'/>
-    </div>
+    <h1 class='margin_b2'>Reviews</h1>
     
     <div class='section-bg' id='reviews-bg'>
     
         <div class='container reviews-container'>
             <div class='image-container'>
-                <img src='images/placeholder.png'/>
+                <img src='images/reviews_pic01.gif'/>
             </div>
             <div class='text-container'>
                 <h2>Title</h2>
                 <h3>Location Name | Date</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
-                <a href='#'><div class='read-more'>Read More</div></a>
+                 <a href='#'><div class='read-more'>READ MORE</div></a>
             </div> <!-- end text-container -->
         </div> <!-- end reviews-container -->
-
+        <hr>
+       <div class='container reviews-container'>
+            <div class='image-container'>
+                <img src='images/reviews_pic02.gif'/>
+            </div>
+            <div class='text-container'>
+                <h2>Title</h2>
+                <h3>Location Name | Date</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
+                 <a href='#'><div class='read-more'>READ MORE</div></a>
+            </div> <!-- end text-container -->
+        </div> <!-- end reviews-container -->
+        <hr>
         <div class='container reviews-container'>
             <div class='image-container'>
-                <img src='images/placeholder.png'/>
+                <img src='images/reviews_pic03.gif'/>
             </div>
             <div class='text-container'>
                 <h2>Title</h2>
                 <h3>Location Name | Date</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
-                <a href='#'><div class='read-more'>Read More</div></a>
+                 <a href='#'><div class='read-more'>READ MORE</div></a>
             </div> <!-- end text-container -->
         </div> <!-- end reviews-container -->
-
-        <div class='container reviews-container'>
-            <div class='image-container'>
-                <img src='images/placeholder.png'/>
-            </div>
-            <div class='text-container'>
-                <h2>Title</h2>
-                <h3>Location Name | Date</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu mattis dui. Curabitur sagittis semper lorem ac congue.</p>
-                <a href='#'><div class='read-more'>Read More</div></a>
-            </div> <!-- end text-container -->
-        </div> <!-- end reviews-container -->
-    
+       <hr>
+       <div class="pre_next"><span class="font_mint">1</span> 2 3 4 5</div>
     </div> <!-- end reviews-bg -->
     
 </main> <!-- end main-section -->
