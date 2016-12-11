807c4e19c7f20bd01422a8ebd8aff5ec653cced1
hw0292 <hw0292@gmail.com>
2016-12-11 00:41:06 -0500

    Add files via upload

--- search_results.php
+++ search_results.php
@@ -11,10 +11,8 @@
 
 <main class='section entireFlax_item' id='search-results-section'>  
     
-    <!--<div class='title-bg'>-->
-        <h1 class='margin_b2'>Search Results</h1>
-    <!--</div>-->
-    
+     <h1 class='margin_b2'>Search Results</h1>
+   
     <div id='search-results-bg'>
         <div class="margin_b2"></div>
         <div class='results-container'>
@@ -23,8 +21,8 @@
             </div> <!-- end results-image -->
             <div class='results-text'>
                 <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <a href='#'><div class='read-more button1'>READ MORE</div></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes...</p>
+                <a href='#'><div class='read-more read_float'>READ MORE</div></a>
             </div> <!-- end results-text -->
         </div> <!-- end results-container -->
         
@@ -34,8 +32,8 @@
             </div> <!-- end results-image -->
             <div class='results-text'>
                 <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <a href='#'><div class='read-more button1'>READ MORE</div></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes...</p>
+                <a href='#'><div class='read-more read_float'>READ MORE</div></a>
             </div> <!-- end results-text -->
         </div> <!-- end results-container -->
         
@@ -45,8 +43,8 @@
             </div> <!-- end results-image -->
             <div class='results-text'>
                 <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <a href='#'><div class='read-more button1'>READ MORE</div></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit massa et dictum scelerisque. Pellentesque ac quam porta, tempor lacus vitae, accumsan libero. Curabitur eu laoreet ante. Morbi a vulputate massa, vitae mollis orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes...</p>
+                <a href='#'><div class='read-more read_float'>READ MORE</div></a>
             </div> <!-- end results-text -->
         </div> <!-- end results-container -->
         
