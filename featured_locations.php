2567b01f17753dc477e3a8689bfbdab1b82fd6ef
hw0292 <hw0292@gmail.com>
2016-12-11 22:05:02 -0500

    Add files via upload
    add featured_locations.php

---
+++ featured_locations.php
@@ -0,0 +1,57 @@
+<!------------ HEADER & NAVIGATION SECTION ------------>
+
+<?php require 'includes/header.php' ?>
+
+<?php require 'includes/nav.php' ?>
+
+<!------------ featured locations SECTION ------------>
+
+
+<main class='section entireFlax_item' id='featured_locations-section'>
+    
+    <h1 class='margin_b2'>Featured Locations</h1>
+    
+    <div class='section-bg' id='reviews-bg'>
+    
+        <div class='container featured-container'>
+            <div class='image-container2 featuredPic1'>
+                <!--<img src='images/reviews_pic01.gif'/>-->
+            </div>
+			<h2>Destination Name</h2>
+            <div class='text-container'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in</p>
+                
+            </div> <!-- end text-container -->
+        </div> <!-- end reviews-container -->
+    </div> <!-- end reviews-bg -->
+	<h1 class='margin_b2'>Other Popular Destinations</h1>
+	<div class="other_popular">
+		<div class="each_destination">
+			<div class="each_popularPic p_pic1"></div>
+			<h2>Destination Name</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+			<a href='#'><div class='read-more'>READ MORE</div></a>
+		</div>
+		<div class="each_destination margin_add1">
+			<div class="each_popularPic p_pic2"></div>
+			<h2>Destination Name</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+			<a href='#'><div class='read-more'>READ MORE</div></a>
+		</div>
+		<div class="each_destination">
+			<div class="each_popularPic p_pic3"></div>
+			<h2>Destination Name</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+			<a href='#'><div class='read-more'>READ MORE</div></a>
+		</div>
+	</div>
+    
+</main> <!-- end main-section -->
+
+
+
+<!------------ FOOTER SECTION ------------>
+
+<?php require 'includes/footer.php' ?>
